921da52748da5bd54b7291c4fb4f147bc66dbc08
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2023-06-20 10:34:36 -0400

    git init

---
+++ src/main/java/com/runicrealms/runicwarps/RunicWarps.java
@@ -0,0 +1,17 @@
+package com.runicrealms.runicwarps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RunicWarps extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
